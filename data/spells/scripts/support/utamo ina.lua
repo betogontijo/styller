fad2e1dc109591a72c34a975557a8f46bd3f531a
betogontijo <betosk08@hotmail.com>
2020-09-16 11:31:34 -0300

    Ignore

--- data/spells/scripts/support/utamo ina.lua
+++ data/spells/scripts/support/utamo ina.lua
@@ -1,11 +1,8 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
-
-local condition = createConditionObject(CONDITION_MANASHIELD)
-setConditionParam(condition, CONDITION_PARAM_TICKS, 0)
-setCombatCondition(combat, condition)
-
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, FALSE)
+setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_MANASHIELD)
+ 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
 end
