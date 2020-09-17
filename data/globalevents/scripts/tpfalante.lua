3b77757fd09ea76fb7b48a9080b5af11ca38626f
heitorgontijo <heitortiburcio9@gmail.com>
2020-09-17 16:49:48 -0300

    Atualizacao Mapa

---
+++ data/globalevents/scripts/tpfalante.lua
@@ -0,0 +1,33 @@
+local config = {
+positions = {
+
+["Teleport"] = { x = 151, y = 52, z = 7 },
+
+["Castle 24h"] = {x = 151, y = 50, z = 7},
+
+["Trainer"] = {x = 155, y = 47, z = 7},
+
+["Area VIP"] = {x = 151, y = 47, z = 7},
+
+
+ 
+
+}
+
+}
+
+ 
+
+function onThink(cid, interval, lastExecution)
+
+for text, pos in pairs(config.positions) do
+
+doSendAnimatedText(pos, text, math.random(100, 255))
+
+end
+
+ 
+
+return TRUE
+
+end
