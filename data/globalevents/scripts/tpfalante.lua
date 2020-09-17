05c5c9bdefd06c3ed7c4e833145fcf2715476480
heitorgontijo <heitortiburcio9@gmail.com>
2020-09-17 17:44:25 -0300

    Atualizacao Mapa

--- data/globalevents/scripts/tpfalante.lua
+++ data/globalevents/scripts/tpfalante.lua
@@ -3,11 +3,14 @@ positions = {
 
 ["Teleport"] = { x = 151, y = 52, z = 7 },
 
-["Castle 24h"] = {x = 151, y = 50, z = 7},
+["Castle24h"] = {x = 151, y = 50, z = 7},
 
 ["Trainer"] = {x = 155, y = 47, z = 7},
 
 ["Area VIP"] = {x = 151, y = 47, z = 7},
+
+["Quests"] = {x = 151, y = 51, z = 7},
+
 
 
  
